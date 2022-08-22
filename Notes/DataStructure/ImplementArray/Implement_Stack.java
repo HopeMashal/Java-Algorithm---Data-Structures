70d4f787bb8dce34a3ba942efa4396571d83b2cc
HopeMashal <mashal.hope@gmail.com>
2022-08-22 17:22:40 +0300

    edit implement stack file

--- Notes/DataStructure/ImplementArray/Implement_Stack.java
+++ Notes/DataStructure/ImplementArray/Implement_Stack.java
@@ -1,5 +1,11 @@
 public class Implement_Stack {
   public static void main(String[] args) {
-    
+    Stack_Array<Integer> stack = new Stack_Array<Integer>(5);
+    stack.push(11);
+    stack.push(12);
+    stack.push(22);
+    System.out.println("Top: "+stack.top);
+    System.out.println(stack.pop());
+    System.out.println("Top: "+stack.top);
   }
 }
